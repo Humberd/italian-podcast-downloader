ba4cea7285d0bae8e571089fd1ce0f6df695b8fd
humberd <humberd2@gmail.com>
2020-06-16 21:09:42 +0200

    Moved definition to another directory

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,11 +2,9 @@ import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.GsonBuilder
 import java.io.File
 
-class Main
-
 fun main() {
     val gson = GsonBuilder().setPrettyPrinting().create()
-    val episodesFile = File("episodes-definition.json")
+    val episodesFile = File("episodes/definition.json")
     val episodes = gson.fromJson<List<Episode>>(episodesFile.reader())
     println(episodes)
     episodesFile.writeText(gson.toJson(episodes))
