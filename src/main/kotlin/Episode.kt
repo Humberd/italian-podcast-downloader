962b7c187836011f3fa65cc34dadada150f94dee
humberd <humberd2@gmail.com>
2020-06-16 23:02:31 +0200

    Downloaded flashcards

--- src/main/kotlin/Episode.kt
+++ src/main/kotlin/Episode.kt
@@ -2,5 +2,6 @@ data class Episode(
     val year: Int,
     val episodeNumber: Int,
     var mp3Downloaded: Boolean,
-    var pdfDownloaded: Boolean
+    var pdfDownloaded: Boolean,
+    var flashCardsDownloaded: Boolean
 )
